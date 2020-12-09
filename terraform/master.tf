3c9f1b46c8390b52b4397ba3ef8849a58c63fe80
Arthur Gilly <agilly@users.noreply.github.com>
2020-12-09 11:11:23 +0700

    Update master.tf

--- terraform/master.tf
+++ terraform/master.tf
@@ -15,7 +15,7 @@ resource "openstack_compute_volume_attach_v2" "spark_volume" {
   volume_id   = var.nfs_volume
 }
 
-resource "openstack_compute_floatingip_associate_v2" "public_ip" {
-  floating_ip = module.networking.floating_ip
-  instance_id = openstack_compute_instance_v2.spark_master.id
-}
+#resource "openstack_compute_floatingip_associate_v2" "public_ip" {
+#  floating_ip = module.networking.floating_ip
+#  instance_id = openstack_compute_instance_v2.spark_master.id
+#}
